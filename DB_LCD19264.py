7f657177b036cc608cb0f03867c7da7c758fb962
r2d2-arduino <54902122+r2d2-arduino@users.noreply.github.com>
2024-10-21 16:32:02 +0300

    Added rotation and some optimizations

--- DB_LCD19264.py
+++ DB_LCD19264.py
@@ -1,9 +1,9 @@
 """
-v0.1.3
+v0.1.4
 
 DB_LCD19264 is a FrameBuffer based MicroPython driver for the graphical
 LiquidCrystal LCD19264 display
-Сonnection used: Data bus
+Сonnection used: Data bus 8-bit
 
 Project path: https://github.com/r2d2-arduino/micropython-lcd19264
 
@@ -19,7 +19,7 @@ Pinout
 5	R/W		H:Read L:Write
 6	E		Enable signal
 7	DB0		}
-...	DB..	} Data bus
+...	DB..	} Data bus 8-bit
 14	DB7		}
 15	CS1		Chip 1 selection L: Active
 17	CS2		Chip 2 selection L: Active
@@ -30,10 +30,6 @@ Pinout
 3 and 19 pins are used to adjust the display contrast through a resistor.
 I use a 1 Mega Ohm variable resistor. Acceptable contrast ~270kOm
 
-*~*~**~*~*~*~**~*~**~*~*~*~*
-*~*~* Support Ukraine! *~*~*
-*~*~**~*~*~*~**~*~**~*~*~*~*
-
 """
 import framebuf
 import time
@@ -42,13 +38,13 @@ from machine import Pin
 LCD_WIDTH = 192
 LCD_HEIGHT = 64
 
-LCD_ADDR         = 0x80
-LCD_DISPLAY_ON   = 0x3F
-LCD_DISPLAY_OFF  = 0x3E
-LCD_ADDR_Y       = 0x40
-LCD_ADDR_X       = 0xB8
-LCD_ADDR_Z       = 0xC0
-LCD_EMPTY        = 0x00
+LCD_ADDR         = const(0x80)
+LCD_DISPLAY_ON   = const(0x3F)
+LCD_DISPLAY_OFF  = const(0x3E)
+LCD_ADDR_Y       = const(0x40)
+LCD_ADDR_X       = const(0xB8)
+LCD_ADDR_Z       = const(0xC0)
+LCD_EMPTY        = const(0x00)
 
 class DB_LCD19264( framebuf.FrameBuffer ):
     def __init__( self, rs, rw, en, rst, cs1, cs2, cs3, db0, db1, db2, db3, db4, db5, db6, db7 ):
@@ -76,12 +72,15 @@ class DB_LCD19264( framebuf.FrameBuffer ):
         self.height = LCD_HEIGHT
         self.width  = LCD_WIDTH
         
+        self._reverse = False
+        
         #order of bites in buffer depending of screen position
         _bufFormat = framebuf.MONO_VLSB
             
         # Initialize the FrameBuffer
         self._bufsize = (self.width * self.height)//8
         self._buffer = bytearray( self._bufsize ) # 8 pixels per Bytes MONO_VLSB (1bit/pixel, 7th bit the topmost pixel)
+        self._memoview = memoryview(self._buffer)
         super().__init__(
                         self._buffer,
                         self.width, # pixels width
@@ -105,7 +104,8 @@ class DB_LCD19264( framebuf.FrameBuffer ):
         self._write_cmd(LCD_ADDR_Z)  # Set begin position (top left corner)
         
     # Send command to display
-    def _write_cmd(self, cmd):
+    @micropython.viper
+    def _write_cmd(self, cmd: int):
         self.rs.off()  # RS = 0 (Command)
         self.rw.off()  # RW = 0 (Write)
         
@@ -117,10 +117,18 @@ class DB_LCD19264( framebuf.FrameBuffer ):
         time.sleep_us(1)
         
     # Write Data Bus
-    def _write_data_bus(self, data):
-        for i in range(8):
-            self.db[i].value(data & (1 << i))
+    @micropython.viper
+    def _write_data_bus(self, data: int):
+        self.db[0].value(data & 1)
+        self.db[1].value(data & (1 << 1))
+        self.db[2].value(data & (1 << 2))
+        self.db[3].value(data & (1 << 3))
+        self.db[4].value(data & (1 << 4))
+        self.db[5].value(data & (1 << 5))
+        self.db[6].value(data & (1 << 6))
+        self.db[7].value(data & (1 << 7))
 
+        
     # Read Data Bus
     def _read_data_bus(self):
         data = 0
@@ -142,7 +150,8 @@ class DB_LCD19264( framebuf.FrameBuffer ):
         return status
 
     # Write data
-    def _write_data(self, data):
+    @micropython.viper
+    def _write_data(self, data: int):
         self.rs.on()  # RS = 1 (Data)
         self.rw.off()  # RW = 0 (Write)
         self._write_data_bus(data)
@@ -167,7 +176,8 @@ class DB_LCD19264( framebuf.FrameBuffer ):
         #self._write_cmd(LCD_DISPLAY_ON) # Display On
 
     # Choose part of display: 1-3
-    def _select_chip(self, chip):
+    @micropython.viper
+    def _select_chip(self, chip: int):
         if chip == 1:
             self.cs1.off()
             self.cs2.on()
@@ -186,64 +196,70 @@ class DB_LCD19264( framebuf.FrameBuffer ):
             self.cs3.off()
             
     # Set address in column: 0-63
-    def _set_address(self, y):
+    @micropython.viper
+    def _set_address(self, y: int):
         self._write_cmd(LCD_ADDR_Y + y)
 
         
     # Set address in row: 0-7
-    def _set_page(self, x):
+    @micropython.viper
+    def _set_page(self, x: int):
         self._write_cmd(LCD_ADDR_X + x)
         
     # Shift start point:0-63
-    def _show_start(self, z):
+    @micropython.viper
+    def _show_start(self, z: int):
         self._write_cmd(LCD_ADDR_Z + z)
-
-    # Send FrameBuffer to lcd (old slovest version)
-    def show2( self ):
-        #start = time.ticks_ms()
-        
-        for chip in range(0, 3):
-            self._select_chip(chip + 1)
-            for page in range(0, 8):
-                self._set_page(page)
-                self._set_address(0)
-                for address in range(0, 64):
-                    pos = address + (page * 192) + (chip * 64)
-                    self._write_data(self._buffer[pos])
-        
-        self._write_cmd(LCD_DISPLAY_ON)
-        
-        #delta = time.ticks_diff(time.ticks_ms(), start)
-        #print(delta/1000)
-        
-        
+    
+    #Rotate display
+    def set_rotation(self, reverse = True):
+        self._reverse = reverse
+            
     # Send FrameBuffer to lcd
+    # reverse - revert 
     def show( self ):
         #start = time.ticks_ms()
 
         self._write_cmd(LCD_ADDR_Z)
-        self._set_address(0)
+        buf = self._memoview
         
         for chip in range(0, 3):
             self._select_chip(chip + 1)
+            self._set_address(0) # Set begin position to 0
             for page in range(0, 8):
                 self._set_page(page)
-                #self._set_address(0) # Set begin position to 0
+                #self._set_address(0) 
                 
                 self.rs.on()  # RS = 1 (Data)
                 self.rw.off()  # RW = 0 (Write)
                 
-                for address in range(0, 64):
-                    pos = address + (page * 192) + (chip * 64)
-                    self._write_data_bus(self._buffer[pos])
-                    
+                posOffset = (page * 192) + (chip * 64)
+                
+                for address in range(0, 64):                    
+                    if self._reverse:
+                        self._write_data_bus(self._reverse_bits(buf[1535 - address - posOffset]))
+                    else:
+                        self._write_data_bus(buf[address + posOffset])
+ 
                     self.en.on()
                     time.sleep_us(1)
                     self.en.off()
                     time.sleep_us(1)                  
                     
         self._write_cmd(LCD_DISPLAY_ON)
- 
-        #delta = time.ticks_diff(time.ticks_ms(), start)
-        #print(delta/1000)        
+        
+        #print(time.ticks_diff(time.ticks_ms(), start)/1000)
 
+    #Reverse bits 0100 0111 => 1110 0010
+    @micropython.viper
+    def _reverse_bits(self, x: int) -> int:#0.378
+        result = 0
+        if x & 1: result |= 1 << 7
+        if (x >> 1) & 1: result |= 1 << 6
+        if (x >> 2) & 1: result |= 1 << 5
+        if (x >> 3) & 1: result |= 1 << 4
+        if (x >> 4) & 1: result |= 1 << 3
+        if (x >> 5) & 1: result |= 1 << 2
+        if (x >> 6) & 1: result |= 1 << 1
+        if (x >> 7) & 1: result |= 1
+        return result
